02338545112ae2b47be2bc9e5b65e2007ebcd9f2
LonaHlongs <unathihlongwana@gmail.com>
2025-04-30 15:11:55 +0200

    Added the search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
-function searchCity(event) {
-    event.preventDefault();
-    let cityInputElement = document.querySelector("#city-input");
-    let weatherCityElement = document.querySelector("#weather-city");
-    weatherCityElement.innerHTML = cityInputElement.value;
+function updateWeather(response) {
+let temperatureElement = document.querySelector("#temperature");
+let temperature = response.data.temperature.current;
+temperatureElement.innerHTML =Math.round(temperature);
+ let weatherCityElement = document.querySelector("#weather-city");
 
+ weatherCityElement.innerHTML = response.data.city;
 }
 
+
+function searchCity(city) {
+let apiKey = "od57b07td52499abaaad3f35534b700e";
+let apiUrl =
+  `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
+
+
+function handleSearchSubmit(event) {
+    event.preventDefault();
+    let cityInput = document.querySelector("#city-input");
+   
+    searchCity(cityInput.value);
+}
+
+
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", searchCity);
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Johannesburg");

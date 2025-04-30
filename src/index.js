e3e63f4da95dda11b9f9ed4f39bdcac840e57616
LonaHlongs <unathihlongwana@gmail.com>
2025-04-30 18:43:33 +0200

    Added more weather data

--- src/index.js
+++ src/index.js
@@ -1,29 +1,47 @@
 function updateWeather(response) {
-let temperatureElement = document.querySelector("#temperature");
-let temperature = response.data.temperature.current;
-temperatureElement.innerHTML =Math.round(temperature);
- let weatherCityElement = document.querySelector("#weather-city");
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let weatherCityElement = document.querySelector("#weather-city");
+  let weatherDescriptionElement = document.querySelector("#weather-description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let weatherDateElement= document.querySelector("#weather-date");
+  let date = new Date(response.data.time * 1000);
 
- weatherCityElement.innerHTML = response.data.city;
+  weatherCityElement.innerHTML = response.data.city;
+  weatherDescriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  weatherDateElement.innerHTML = formatDate(date);
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
+function formatDate(date) {
+  
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
 
 function searchCity(city) {
-let apiKey = "od57b07td52499abaaad3f35534b700e";
-let apiUrl =
-  `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let apiKey = "od57b07td52499abaaad3f35534b700e";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(updateWeather);
 }
 
-
-
 function handleSearchSubmit(event) {
-    event.preventDefault();
-    let cityInput = document.querySelector("#city-input");
-   
-    searchCity(cityInput.value);
-}
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
 
+  searchCity(cityInput.value);
+}
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);

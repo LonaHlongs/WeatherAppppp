de5cd7d0b345ecf387f4820792408b10cc17a60c
LonaHlongs <unathihlongwana@gmail.com>
2025-05-15 16:53:45 +0200

    Adjusted the footer

--- src/index.js
+++ src/index.js
@@ -61,27 +61,26 @@ function handleSearchSubmit(event) {
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  
+
   return days[date.getDay()];
 }
 
 function getForecast(city) {
-let apiKey = "od57b07td52499abaaad3f35534b700e";
-let apiUrl =
-  `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  let apiKey = "od57b07td52499abaaad3f35534b700e";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
 function displayForecast(response) {
-let forecastElement = document.querySelector("#forecast");
+  let forecastElement = document.querySelector("#forecast");
 
-let forecastHtml = "";
+  let forecastHtml = "";
 
-response.data.daily.forEach(function (day, index) {
-  if (index < 5) {
-forecastHtml =
-  forecastHtml +
-  `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
    <div class="weather-forecast-day">
     <div class="weather-forecast-date">${formatDay(day.time)}</div>
     <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
@@ -95,18 +94,13 @@ forecastHtml =
 </div>
 </div>
 `;
-  }
-});
+    }
+  });
 
-
-forecastElement.innerHTML = forecastHtml;
+  forecastElement.innerHTML = forecastHtml;
 }
-
-
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Johannesburg");
-
-
